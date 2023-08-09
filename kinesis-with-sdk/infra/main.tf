ef19b36efd6bde1706b47fe1a7ac73f4848f86dc
Victor Moraes <vhugo006@gmail.com>
2023-08-09 15:04:43 -0300

    Configure consumer and streamer of kinesis using SDK

--- kinesis-with-sdk/infra/main.tf
+++ kinesis-with-sdk/infra/main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_kinesis_stream" "data_portfolio_stream" {
-  name             = "DatPortfolioKinesisStream"
+  name             = "DataPortfolioKinesisStream"
   shard_count      = 1
 }
